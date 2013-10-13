923afce128dd8b95748729c44ca1ddfbc3eae28b
simonstastny <stastny.simon@gmail.com>
2013-10-13 09:20:36 +0200

    implementing search by coordinates

---
+++ test_population_script.sql
@@ -0,0 +1,9 @@
+INSERT INTO picture (pictureid, wall_model_wall_id, url) VALUES
+(1,	1,	'http://upload.wikimedia.org/wikipedia/en/8/8c/Schema_Orloj_pragueorlojhzenilc.jpg'),
+(2,	1,	'http://upload.wikimedia.org/wikipedia/commons/7/79/Prazsky_orloj_celkovy_pohled.jpg');
+
+
+
+INSERT INTO wall (wall_id, latitude, longitude, name) VALUES
+(2,	63.427255000000002,	10.396545,	"Nidarosdomen"),
+(1,	50.086910000000003,	14.420795,	"Orloj");
