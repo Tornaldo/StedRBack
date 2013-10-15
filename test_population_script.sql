8f31234a979093d5403aa66dd7cde0e3e8df9597
simonstastny <stastny.simon@gmail.com>
2013-10-15 17:48:47 +0200

    new walls and pictures in the population script

--- test_population_script.sql
+++ test_population_script.sql
@@ -1,9 +1,22 @@
 INSERT INTO picture (pictureid, wall_model_wall_id, url) VALUES
 (1,	1,	'http://upload.wikimedia.org/wikipedia/en/8/8c/Schema_Orloj_pragueorlojhzenilc.jpg'),
-(2,	1,	'http://upload.wikimedia.org/wikipedia/commons/7/79/Prazsky_orloj_celkovy_pohled.jpg');
+(2,	1,	'http://upload.wikimedia.org/wikipedia/commons/7/79/Prazsky_orloj_celkovy_pohled.jpg'),
+(3,	2,	'http://upload.wikimedia.org/wikipedia/commons/b/b3/Nidarosdomen_ryjord_1907.png'),
+(4,	2,	'http://www.nidarosdomen.no/Images/TopImages/1.jpg'),
+(5,	2,	'http://konsertoversikt.no/placeimages/nidarosdomen.jpg'),
+(6,	8,	'http://upload.wikimedia.org/wikipedia/commons/4/4e/Chehel_Sotoon.jpg')
+;
 
 
 
 INSERT INTO wall (wall_id, latitude, longitude, name) VALUES
-(2,	63.427255000000002,	10.396545,	"Nidarosdomen"),
-(1,	50.086910000000003,	14.420795,	"Orloj");
+(1,	50.086910000000003,	14.420795,	'Orloj'),
+(2,	63.427255000000002,	10.396545,	'Nidarosdomen'),
+(3,	63.445338,			10.420811,	'Gigaphone'),
+(4,	63.419599,			10.401843,	'Hovedbygget Gløshaugen'),
+(5,	63.428085,			10.402443,	'Gamle bybru'),
+(6,	35.699736,			51.338103,	'Azadi square'),
+(7,	35.710261,			51.392262,	'Park Laleh'),
+(8,	32.657504,			51.672381,	'Chehel Sotun'),
+(9,	32.64502,			51.667596,	'Si-o-seh Pol')
+;
