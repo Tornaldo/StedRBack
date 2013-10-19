0a0daf8584d832d39816f800b5757ce123679c9f
simonstastny <stastny.simon@gmail.com>
2013-10-19 13:20:52 +0200

    guava dependency added

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object ApplicationBuild extends Build {
     javaCore,
     javaJdbc,
     javaEbean,
-    "com.google.guava" % "guava" % "14.0" % "guava14",
+    "com.google.guava" % "guava" % "14.0",
     "org.jsoup" % "jsoup" % "1.6.3"
   )
 
