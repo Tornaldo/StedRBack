99f8447716018ec3febdfee38bd8c36c11913902
Knutner <knut.nerga@gmail.com>
2013-10-23 18:17:44 +0200

    Update StedrConstants.java

--- app/services/StedrConstants.java
+++ app/services/StedrConstants.java
@@ -6,5 +6,7 @@ public class StedrConstants {
 	public static final String FLICKR_API_KEY = "cd04f142470e7de7c992b3a3b140f636";
 	
 	public static final String STEDR_GROUP_ID = "2297124%40N25"; // escaped
+	
+	//access_token=623771306.1fb234f.09aa9355cc8e469f8839d18385f719d5 
 
 }
